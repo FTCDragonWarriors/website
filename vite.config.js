2d4f497ec5251a9457a0b44e8d527536ab2f9f81
FTCDragonWarriors <ftcdragonwarriors@gmail.com>
2026-02-03 13:51:30 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/ftcdragonwarrior/',
+  base: '/website/',
   build: {
     rollupOptions: {
       input: {
